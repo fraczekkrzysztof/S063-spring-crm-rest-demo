35124f9aaac3344ac97d1662a8ac7a84100378cb
fraczekkrzysztof <fraczekkrzysztof@o2.pl>
2018-08-07 18:51:56 +0200

    ExceptionHandler and PostMapping
    Add an exception handler and PostMapping for adding new customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,19 +17,34 @@ import com.luv2code.springdemo.service.CustomerService;
 @RequestMapping("/api")
 public class CustomerRestController {
 
-	//autowire the CustomerService
+	// autowire the CustomerService
 	@Autowired
 	private CustomerService customerService;
-	
-	//add mapping for GET /customers
+
+	// add mapping for GET /customers
 	@GetMapping("/customers")
-	public List<Customer> getCustomers(){
-		return customerService.getCustomers();		
+	public List<Customer> getCustomers() {
+		return customerService.getCustomers();
 	}
-	
-	//add mapping for GET /customer/{customerId}
+
+	// add mapping for GET /customer/{customerId}
 	@GetMapping("/customers/{customerId}")
 	public Customer getCutomer(@PathVariable int customerId) {
-		return customerService.getCustomer(customerId);
+		Customer theCustomer = customerService.getCustomer(customerId);
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		return theCustomer;
+	}
+
+	// add mapping for POST /customer - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		// also just in case the pass an id in JSON ... set id to 0
+		// this force a save of new item ... instead of update
+		theCustomer.setId(0); // bo metoda hibernate SaveOrUpdate gdy id jest null lub 0 to uznaje, że rekord
+								// jest dodawany - wykonuje insert
+		customerService.saveCustomer(theCustomer);
+		return theCustomer;
 	}
 }
